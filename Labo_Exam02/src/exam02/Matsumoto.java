4e1ee3a9c9bee9985019c1f25ef9f54682019bd5
shigure242 <shigure242@yahoo.co.jp>
2013-07-06 19:08:16 +0900

    Matsumoto追加

--- Labo_Exam02/src/exam02/Matsumoto.java
+++ Labo_Exam02/src/exam02/Matsumoto.java
@@ -1,7 +1,41 @@
 package exam02;
 
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Matsumoto extends JPanel {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
+import net.miginfocom.swing.MigLayout;
+
+public class Matsumoto extends JPanel implements ActionListener {
+	static JButton in = new JButton("下に送信");
+	static JTextPane textIN = new JTextPane();
+	static JTextPane textOUT = new JTextPane();
+	// パーツの用意
+
+	public Matsumoto() {
+		this.setLayout(new MigLayout("", "[grow]", "[][grow][][grow]"));
+
+		this.add(new JLabel("下に送信したいメッセージを入力してください。"), "wrap,c");
+
+		this.add(textIN, "grow,wrap");
+
+		this.add(in, "grow,wrap");
+		in.addActionListener(this);
+
+		this.add(textOUT, "grow");
+		// セット
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		textOUT.setText(textIN.getText());
+		textIN.setText("");
+		// アクション処理
+	}
 
 }
