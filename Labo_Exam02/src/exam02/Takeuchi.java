a41a87b195cd02a6414652d09e7fdf56a60a56f6
killinsun <noneck.0311@gmail.com>
2013-07-06 22:28:12 +0900

    Killinsun Committed for exam02

--- Labo_Exam02/src/exam02/Takeuchi.java
+++ Labo_Exam02/src/exam02/Takeuchi.java
@@ -1,7 +1,33 @@
 package exam02;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import net.miginfocom.swing.MigLayout;
 
 public class Takeuchi extends JPanel {
+	public Takeuchi(){
+		
+		this.setLayout(new MigLayout("", "[grow][]", "[grow]"));
+		
+		
+		//PrintImageTest.
+		JLabel faceLabel = new JLabel();
+		ImageIcon face = new ImageIcon("./data/face.png");
+	    faceLabel.setIcon(face);
+		this.add(faceLabel, "cell 0 0,grow");
+		
+		JLabel faceLabel2 = new JLabel();
+		ImageIcon face2 = new ImageIcon("./data/face.png");
+	    faceLabel2.setIcon(face2);
+		this.add(faceLabel2, "cell 1 0,grow");
+
+
+		
+	}
 
 }
