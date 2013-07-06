e5556915dba99900ecfe45e5540e550db8fe48d9
blacky <>
2013-07-07 02:56:40 +0900

    RuntimeExceptionを受け取る

--- Labo_Exam02/src/exam02/MainFrame.java
+++ Labo_Exam02/src/exam02/MainFrame.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -24,15 +25,22 @@ public class MainFrame extends JFrame {
 		getContentPane().setLayout(growLayout);
 		
 		// 各個人で制作するJPanelをListにまとめる
-		List<JPanel> panels = Arrays.asList(
-				new Anzai(),
-				new Ishikawa(),
-				new Kadowaki(),
-				new Takeuchi(),
-				new Matsumoto(),
-				new Murata(),
-				new Morimoto()
-		);
+		List<JPanel> panels = null;
+		try {
+			panels = Arrays.asList(
+					new Anzai(),
+					new Ishikawa(),
+					new Kadowaki(),
+					new Takeuchi(),
+					new Matsumoto(),
+					new Murata(),
+					new Morimoto()
+			);
+		} catch (RuntimeException e) {
+			// あいやくんのRuntimeExceptionを受け取る
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			System.exit(0);
+		}
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
