69252005f6489cd9254cf4c14650e1f62c52b145
Owner <Owner@Owner-PC>
2013-07-08 00:10:14 +0900

    morimoto

--- Labo_Exam02/src/exam02/Morimoto.java
+++ Labo_Exam02/src/exam02/Morimoto.java
@@ -1,7 +1,25 @@
 package exam02;
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
-public class Morimoto extends JPanel {
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
 
-}
+public class Morimoto extends JPanel implements ActionListener{
+	public Morinoto(){
+		
+		JPanel p = new JPanel();
+		Container contentPane = getContentPane();
+	    contentPane.add(p, BorderLayout.CENTER);
+	    
+	    Button b = new Button("赤");
+	    b.addActionListener(this);
+	    add(b);
+	    setSize(100,50);
+	    show();
+	}
+	public void actionPerfomed(ActionEvent e){
+		p.setColor(Color.RED);
+	}
